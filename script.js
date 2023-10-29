41a221b2f4f0b88c36e368b92a83065fc5af097f
alicia3194 <aliciacortinez@gmail.com>
2023-10-29 21:53:38 +0100

    styless and API

--- script.js
+++ script.js
@@ -1,17 +1,75 @@
-async function getCategoriesFoodPreview() {
-  try {
-    const response = await fetch(
-      "https://www.themealdb.com/api/json/v1/1/random.php"
-    );
-    const data = await response.json();
-    const food = data.meals;
+// 10 platos aleatorios para la seccion de platos populares
 
-    console.log(data, food);
+async function getRandomFoodPreview() {
+  try {
+    const numRandomMeals = 10;
+    const randomMeals = [];
+
+    for (let i = 0; i < numRandomMeals; i++) {
+      const response = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/random.php"
+      );
+
+      if (response.status === 200) {
+        const data = await response.json();
+        const food = data.meals[0];
+
+        const trendingPreviewFoodContainer = document.querySelector(
+          "#trendingPreview .trendingPreview-foodList"
+        );
+
+        const foodContainer = document.createElement("div");
+        foodContainer.classList.add("food-container");
+
+        const foodImg = document.createElement("img");
+        foodImg.classList.add("food-img");
+        foodImg.setAttribute("alt", food.strMeal);
+        foodImg.setAttribute("src", food.strMealThumb);
+
+        foodContainer.appendChild(foodImg);
+        trendingPreviewFoodContainer.appendChild(foodContainer);
+
+        randomMeals.push(food);
+      } else {
+        alert("Error en la solicitud a la API");
+      }
+    }
+    console.log("Comidas aleatorias:", randomMeals);
   } catch (error) {
     console.error(error);
   }
 }
-getCategoriesFoodPreview();
+
+// seccion para las categorias de platos
+
+async function getCategoriesPreview() {
+  try {
+    const response = await fetch(
+      "https://www.themealdb.com/api/json/v1/1/categories.php"
+    );
+
+    if (response.status === 200) {
+      const data = await response.json();
+      const categories = data.categories;
+
+      categories.forEach((category, index) => {
+        const categoryTitle = category.strCategory;
+        const categoryContainer = document.querySelector(
+          `.category-container:nth-child(${index + 1}) .category-title`
+        );
+        categoryContainer.textContent = categoryTitle;
+      });
+    } else {
+      alert("Error en la solicitud a la API");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+//Llamar a las funciones
+getRandomFoodPreview();
+getCategoriesPreview();
 
 //POR CATEGORIAS
 
